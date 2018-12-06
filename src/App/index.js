0e35cd97e1f587fb20bad04f3b08365240bf1e72
mildk123 <mildk123@gmail.com>
2018-12-06 17:13:31 +0500

    initial commit

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Auth from '../Screens/Auth'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App js</h1>
+        <Auth />
+      </div>
+    )
+  }
+}
+
+export default App;
